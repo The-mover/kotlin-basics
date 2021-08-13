14fa7fe086164e42ebb5828f7769bd7d3d0f93b6
ashraful <ashraful.rahi@gmail.com>
2021-08-13 12:27:39 +0600

    Added a simple retrofit project with kotlin

---
+++ network-call/app/src/main/java/com/example/networkcall/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.networkcall
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var headingTV: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        headingTV = findViewById(R.id.heading_tv)
+
+        getMyData()
+    }
+
+    private fun getMyData() {
+       val retrofitBuilder = Retrofit.Builder()
+           .addConverterFactory(GsonConverterFactory.create())
+           .baseUrl(BASE_URL)
+           .build()
+           .create(ApiInterface::class.java)
+
+        val retrofitData = retrofitBuilder.getData()
+
+        retrofitData.enqueue(object : Callback<List<MyDataItem>?> {
+            override fun onResponse(
+                call: Call<List<MyDataItem>?>,
+                response: Response<List<MyDataItem>?>
+            ) {
+                val responseBody = response.body()!!
+                val myStringBuilder = StringBuilder()
+
+                for(myData in responseBody) {
+                    myStringBuilder.append(myData.title)
+                    myStringBuilder.append("\n")
+                }
+
+                headingTV.text = myStringBuilder
+            }
+
+            override fun onFailure(call: Call<List<MyDataItem>?>, t: Throwable) {
+                Log.d("MainActivity", "onFailure: ${t.message}")
+            }
+        })
+    }
+}
---
+++ network-call/app/src/main/java/com/example/networkcall/MyData.kt
@@ -0,0 +1,3 @@
+package com.example.networkcall
+
+class MyData : ArrayList<MyDataItem>()
